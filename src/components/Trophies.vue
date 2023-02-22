46c37e1b2cc805b827969535d76a5f2782ac469f
Jacorb90 <39597610+Jacorb90@users.noreply.github.com>
2023-02-22 16:51:54 -0500

    fix gen buy

--- src/components/Trophies.vue
+++ src/components/Trophies.vue
@@ -9,15 +9,15 @@
                 <h4>{{ENEMY_DATA[id].name}} [{{formatWhole(value)}}]</h4>
                 {{fromEnemyData(id, "trophyDesc")?.(0) ?? "???"}}
             </button><br>
-            <button v-if="player.bestiary[Number(id)+6]!==undefined" :class="{bestiaryDiv: true, short: true, unlocked: true, canbuy: Decimal.gte(player.bestiary[Number(id)+6]||0, getTrophyGenUpgCost(id))}" v-on:click="() => buyTrophyGenUpg(id)">
+            <button v-if="player.bestiary[Number(id)+6]!==undefined" :class="{bestiaryDiv: true, short: true, unlocked: true, canbuy: Decimal.gte(player.bestiary[Number(id)+6]||0, getTrophyGenUpgCost(Number(id)))}" @click="() => buyTrophyGenUpg(Number(id))">
                 <h4 style="margin-top: 2px;">+{{formatWhole(getTrophyGen(Number(id)))}} {{ENEMY_DATA[id].name}} Trophies/sec</h4>
                 Cost: {{formatWhole(getTrophyGenUpgCost(Number(id)))}} {{(ENEMY_DATA[Number(id)+6]!==undefined)?(ENEMY_DATA[Number(id)+6].name+" Trophies"):"???"}}
             </button><br>
-            <button :tooltip="'Sacrificed Trophies add to Trophy effects, but are permanent (i.e. don\'t have to be selected to work). \n Cost: 50% of Trophies (req amt: ' + formatWhole(trophySacReq(id)) + ') \n Loss Rate: 50%'" v-if="trophySacUnl(id)" :class="{bestiaryDiv: true, tall: true, unlocked: true, canbuy: canTrophySac(id)}" @click="() => trophySac(id)">
+            <button :tooltip="'Sacrificed Trophies add to Trophy effects, but are permanent (i.e. don\'t have to be selected to work). \n Cost: 50% of Trophies (req amt: ' + formatWhole(trophySacReq(Number(id))) + ') \n Loss Rate: 50%'" v-if="trophySacUnl(Number(id))" :class="{bestiaryDiv: true, tall: true, unlocked: true, canbuy: canTrophySac(Number(id))}" @click="() => trophySac(Number(id))">
                 <h4 style="margin-top: 2px;">{{formatWhole(player.trophySac[id] ?? 0)}} Sacrificed {{ENEMY_DATA[id].name}} Trophies</h4>
                 {{(fromEnemyData(id, "sacDesc") ?? fromEnemyData(id, "trophyDesc"))?.(1) ?? "???"}}
             </button><br>
-            <button v-if="trophySacUnl(id)" :class="{ binary: true, enabled: !(player.trophySacDisabled[id] ?? false), disabled: player.trophySacDisabled[id] ?? false }" @click="() => toggleSac(id)">
+            <button v-if="trophySacUnl(Number(id))" :class="{ binary: true, enabled: !(player.trophySacDisabled[id] ?? false), disabled: player.trophySacDisabled[id] ?? false }" @click="() => toggleSac(Number(id))">
                 {{ player.trophySacDisabled[id] ? "OFF" : "ON" }}
             </button>
         </div>
