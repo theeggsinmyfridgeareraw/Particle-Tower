b0a6e3c653fa49f31f671f20062cde230cb2a596
Jacorb90 <39597610+Jacorb90@users.noreply.github.com>
2023-02-22 16:39:48 -0500

    trophie

--- src/components/Trophies.vue
+++ src/components/Trophies.vue
@@ -5,13 +5,13 @@
     <br><br>
     <div class="row" style="display: flex; margin-bottom: 10px;" v-for="r in Math.ceil(Object.keys(player.bestiary).length/6)">
         <div v-for="(value, id) in reducedBestiary(r)">
-            <button :class="{bestiaryDiv: true, unlocked: true, enabled: player.bestiaryChosen[id]}" @click="() => toggleTrophy(id)">
+            <button :class="{bestiaryDiv: true, unlocked: true, enabled: player.bestiaryChosen[id]}" @click="() => toggleTrophy(Number(id))">
                 <h4>{{ENEMY_DATA[id].name}} [{{formatWhole(value)}}]</h4>
                 {{fromEnemyData(id, "trophyDesc")?.(0) ?? "???"}}
             </button><br>
             <button v-if="player.bestiary[Number(id)+6]!==undefined" :class="{bestiaryDiv: true, short: true, unlocked: true, canbuy: Decimal.gte(player.bestiary[Number(id)+6]||0, getTrophyGenUpgCost(id))}" v-on:click="() => buyTrophyGenUpg(id)">
-                <h4 style="margin-top: 2px;">+{{formatWhole(getTrophyGen(id))}} {{ENEMY_DATA[id].name}} Trophies/sec</h4>
-                Cost: {{formatWhole(getTrophyGenUpgCost(id))}} {{(ENEMY_DATA[Number(id)+6]!==undefined)?(ENEMY_DATA[Number(id)+6].name+" Trophies"):"???"}}
+                <h4 style="margin-top: 2px;">+{{formatWhole(getTrophyGen(Number(id)))}} {{ENEMY_DATA[id].name}} Trophies/sec</h4>
+                Cost: {{formatWhole(getTrophyGenUpgCost(Number(id)))}} {{(ENEMY_DATA[Number(id)+6]!==undefined)?(ENEMY_DATA[Number(id)+6].name+" Trophies"):"???"}}
             </button><br>
             <button :tooltip="'Sacrificed Trophies add to Trophy effects, but are permanent (i.e. don\'t have to be selected to work). \n Cost: 50% of Trophies (req amt: ' + formatWhole(trophySacReq(id)) + ') \n Loss Rate: 50%'" v-if="trophySacUnl(id)" :class="{bestiaryDiv: true, tall: true, unlocked: true, canbuy: canTrophySac(id)}" @click="() => trophySac(id)">
                 <h4 style="margin-top: 2px;">{{formatWhole(player.trophySac[id] ?? 0)}} Sacrificed {{ENEMY_DATA[id].name}} Trophies</h4>
